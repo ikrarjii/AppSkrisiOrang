9d0f2d2610fa91a83a41291a88d6f76dfdc796b2
ikrar <ikrar1469@gmail.com>
2023-01-31 08:48:44 +0700

    upadate barcode

--- lib/presentation/pages/Check.dart
+++ lib/presentation/pages/Check.dart
@@ -22,35 +22,74 @@ class CheckPage extends StatefulWidget {
 
 class _CheckPageState extends State<CheckPage> {
   Future<void> scanQR() async {
-    // String barcodeScanRes;
-    // // int _StatusGaji = 123456;
-    // // Platform messages may fail, so we use a try/catch PlatformException.
+    String barcodeScanRes;
+    // int _StatusGaji = 123456;
+    // Platform messages may fail, so we use a try/catch PlatformException.
 
-    // barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-    //     '#ff6666', 'Cancel', true, ScanMode.QR);
-    // await scanner.scan();
-    final user = await FirebaseAuth.instance.currentUser;
+    // final user = await FirebaseAuth.instance.currentUser;
 
-    String uid = await user!.uid;
+    // String uid = await user!.uid;'
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String uid = auth.currentUser!.uid;
 
     FirebaseFirestore firestore = FirebaseFirestore.instance;
 
     CollectionReference<Map<String, dynamic>> cPresent =
         await firestore.collection("users").doc(uid).collection("present");
 
+    final user = firestore.collection("users");
+
     QuerySnapshot<Map<String, dynamic>> snapPrensent = await cPresent.get();
     // print("ffff");
     // print("masuk = ${snapPrensent.docs.length}");
+
+    // print(uid);
     DateTime now = DateTime.now();
     String todayDocID = DateFormat().add_yMd().format(now).replaceAll("/", "-");
-    // print(todayDocID);
 
     if (snapPrensent.docs.length == 0) {
       await cPresent.doc(todayDocID).set({
+        "date": now.toIso8601String(),
         "masuk": {
           "date": now.toIso8601String(),
+          // "user": user!.email,
         }
       });
+    } else {
+      DocumentSnapshot<Map<String, dynamic>> absenn =
+          await cPresent.doc(todayDocID).get();
+
+      print(absenn.exists);
+
+      if (absenn.exists == true) {
+        Map<String, dynamic>? dataPresentTod = absenn.data();
+
+        if (dataPresentTod?["keluar"] != null) {
+          Utils.showSnackBar(
+              "Sukses Sudah Absen Masuk && Keluar.", Colors.green);
+        } else {
+          //absen Keluar
+          barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+              '#ff6666', 'Cancel', true, ScanMode.QR);
+          await scanner.scan();
+          await cPresent.doc(todayDocID).update({
+            "keluar": {
+              "date": now.toIso8601String(),
+            }
+          });
+        }
+      } else {
+        //masuk
+        barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#ff6666', 'Cancel', true, ScanMode.QR);
+        await scanner.scan();
+        await cPresent.doc(todayDocID).set({
+          "date": now.toIso8601String(),
+          "masuk": {
+            "date": now.toIso8601String(),
+          }
+        });
+      }
     }
 
     // showDialog(
@@ -195,268 +234,25 @@ class _CheckPageState extends State<CheckPage> {
   Widget build(BuildContext context) {
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     final user = FirebaseAuth.instance.currentUser;
-
-    // String nama = data['nama'];
-
-    // final _alamat = "";
-    // final _created_at = "";
-    // final _device_id = "";
-    // final _email = "";
-    // final _nama = "";
-    // final _no_hp = "";
-    // final _no_rekening = "";
-    // final _uid = "";
-
-    // String nama = data['nama'];
-    // int umur = data['umur'];
-    // FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-    // var data = await Firestore.instance.collection('nama_collection').document('document_id').get();
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String uid = auth.currentUser!.uid;
 
     return Scaffold(
       body: StreamBuilder<QuerySnapshot>(
           stream: firestore
               .collection("users")
-              .where("email", isEqualTo: user!.email)
-              .where("tanggal",
-                  isEqualTo:
-                      DateFormat("EEEE, dd MMMM yyyy").format(DateTime.now()))
+              .doc(uid)
+              .collection("presence")
+              .orderBy("date")
+              .limitToLast(5)
               .snapshots(),
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              bool isCheckIn = snapshot.data!.docs.length > 0;
-              bool isCheckOut = snapshot.data!.docs.length > 0 &&
-                  snapshot.data!.docs[0]["check_out"] != "";
-              return Column(
-                children: <Widget>[
-                  Container(
-                    child: Stack(
-                      children: [
-                        Container(
-                            child: Image.asset(
-                          Gambar.lmbur,
-                          fit: BoxFit.cover,
-                          width: double.infinity,
-                          height: MediaQuery.of(context).size.height * 0.4,
-                        )),
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          child: Container(),
-                        ),
-                        Container(
-                          margin: EdgeInsets.symmetric(vertical: 20),
-                          width: double.infinity,
-                          padding: EdgeInsets.only(),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    IconButton(
-                                        icon: Icon(Icons.arrow_back),
-                                        color: Warna.putih,
-                                        onPressed: () {
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      MyPages()));
-                                        }),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Title(
-                                color: Warna.putih,
-                                child: Text(
-                                  (DateFormat('KK:mm').format(DateTime.now())),
-                                  style: TextStyle(
-                                    color: Warna.putih,
-                                    fontSize: 32,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Title(
-                                color: Warna.hijau2,
-                                child: Text(
-                                  (DateFormat('dd MMMM yyyy')
-                                      .format(DateTime.now())),
-                                  style: TextStyle(
-                                    color: Warna.putih,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Container(
-                            margin: EdgeInsets.only(top: 180, bottom: 10),
-                            width: double.infinity,
-                            padding: EdgeInsets.all(25),
-                            child: !isCheckIn
-                                ? ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      primary: Warna.kuning,
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 20),
-                                    ),
-                                    child: Text("Check In"),
-                                    onPressed: () async {
-                                      await scanQR();
+            return Container();
 
-                                      // Navigator.push(context,
-                                      //     MaterialPageRoute(builder: (context) => Csan()));
-                                    },
-                                  )
-                                : isCheckIn && !isCheckOut
-                                    ? ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                          primary: Warna.mrah,
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 20),
-                                        ),
-                                        child: Text("Check Out"),
-                                        onPressed: () {
-                                          // scanQROut(snapshot.data!.docs[0].id);
-                                          // final user =
-                                          //     FirebaseAuth.instance.currentUser;
-                                          // var id = user.uid;
-                                          //  DateTime noww = DateTime.now();
-                                          //  String nama = docUser.docs[0]["nama"];
+            //fff
 
-                                          // Navigator.push(context,
-                                          //     MaterialPageRoute(builder: (context) => Csan()));
-                                        },
-                                      )
-                                    : Container())
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          DocumentSnapshot data = snapshot.data!.docs[index];
-                          return ItemCard1(data);
-                        }),
-                  ),
-                ],
-              );
-            }
-
-            return CircularProgressIndicator();
             //batass
           }),
     );
   }
-}
-
-Container ItemCard1(DocumentSnapshot? data) {
-  return Container(
-    alignment: Alignment.centerLeft,
-    margin: EdgeInsets.all(20),
-    padding: EdgeInsets.symmetric(horizontal: 20),
-    height: 150,
-    width: double.infinity,
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(25), //border corner radius
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.5), //color of shadow
-          spreadRadius: 1, //spread radius
-          blurRadius: 7, // blur radius
-          offset: Offset(0, 2),
-        ),
-      ],
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          children: [
-            Icon(
-              Icons.date_range,
-              color: Warna.htam,
-              size: 20.0,
-            ),
-            Text(
-              data!["tanggal"],
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-                color: Warna.abuabu,
-              ),
-            ),
-          ],
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        Row(children: [
-          Text(
-            "Check In",
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w600,
-              color: Warna.htam,
-            ),
-          ),
-          SizedBox(
-            width: 38,
-          ),
-          Text(
-            DateFormat("HH mm").format(data["check_in"].toDate()),
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w600,
-              color: Warna.abuabu,
-            ),
-          ),
-        ]),
-        SizedBox(
-          height: 7,
-        ),
-        Row(
-          children: [
-            Text(
-              "Check out",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w600,
-                color: Warna.htam,
-              ),
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Text(
-              data["check_out"] != ""
-                  ? DateFormat("HH mm").format(data["check_out"].toDate())
-                  : "-",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w600,
-                color: Warna.abuabu,
-              ),
-            ),
-          ],
-        )
-      ],
-    ),
-  );
 }
